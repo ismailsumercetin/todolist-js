4cd4cc5547df8cabf4bb5f899085c659c7a981db
Sumer Cetin <sumercetin3@gmail.com>
2020-08-13 01:55:49 +0300

    Removing from dom (not from array yet)

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const tasks = [
   { id: 5, owner: 1, text: "Clean the house" }
 ];
 
-const selectedIndex = 1;
+const selectedIndex = 5;
 
 var ulElement = document.getElementById("todoList");
 var todoOwnersSelectElement = document.getElementById("todoOwners");
@@ -22,9 +22,9 @@ var addTop = document.getElementById("addToTop");
 var addBottom = document.getElementById("addToBottom");
 var titleElement = document.getElementById("todoOwnerName");
 var newTaskTextbox = document.getElementById("newTask");
+var liElements;
 
 const setSelectedOwnerName = function () {
-  console.log();
   titleElement.textContent =
     todoOwnersSelectElement.options[todoOwnersSelectElement.selectedIndex].text;
 };
@@ -52,22 +52,26 @@ const populateTask = function () {
   tasksOfOwner.map((task) => {
     var listElement = document.createElement("li");
     listElement.textContent = task.text;
+    listElement.setAttribute("taskId", task.id);
     return ulElement.appendChild(listElement);
   });
+  liElements = document.getElementsByTagName("li");
 };
 
 const addNewTask = function (place) {
   var newTaskText = newTaskTextbox.value;
+  var newTaskId = ++tasks[tasks.length - 1].id;
 
   //create object
   var newTask = {};
-  newTask.id = ++tasks[tasks.length - 1].id;
-  newTask.owner = Number(todoOwnersSelectElement.value);
+  newTask.id = newTaskId;
   newTask.text = newTaskText;
+  newTask.owner = Number(todoOwnersSelectElement.value);
 
-  //create dom element
+  //create dom element (burası problemli)
   var listElement = document.createElement("li");
   listElement.textContent = newTaskText;
+  listElement.setAttribute("taskId", newTaskId);
 
   if (listElement.textContent.length !== 0) {
     if (place === "top") {
@@ -86,6 +90,36 @@ const addNewTask = function (place) {
   }
 };
 
+const remove = function () {
+  //from array
+  /* is there any way
+  var taskDeleted = tasks.find((task) => {
+    return task.id === Number(this.getAttribute("taskId"));
+  });
+  console.log(taskDeleted)
+  console.log(tasks.splice(taskDeleted.id, 1))
+  console.log(tasks)
+  */
+  //from dom
+  this.remove();
+};
+//make all tasks removable
+const makeTasksRemovable = function () {
+  [...liElements].forEach((liElement) => {
+    liElement.addEventListener("click", remove, false);
+  });
+  /*
+  for (let i = 0; i < liElements.length; i++) {
+    liElements[i].addEventListener("click", remove, false);
+  }
+  */
+};
+
+//make single task removable (new added tasks)
+const makeATaskRemovable = function (addedTask) {
+  addedTask.addEventListener("click", remove, false);
+};
+
 //handling dynamic title
 todoOwnersSelectElement.addEventListener("change", (e) => {
   setSelectedOwnerName();
@@ -96,17 +130,21 @@ todoOwnersSelectElement.addEventListener("change", (e) => {
   //remove tasks
   ulElement.innerHTML = "";
   populateTask();
+  makeTasksRemovable();
 });
 
 addTop.addEventListener("click", (e) => {
   e.preventDefault();
   addNewTask("top");
+  makeATaskRemovable(ulElement.firstChild);
 });
 
 addBottom.addEventListener("click", (e) => {
   e.preventDefault();
   addNewTask("bottom");
+  makeATaskRemovable(ulElement.lastChild);
 });
 
 populateTaskOwners();
 populateTask();
+makeTasksRemovable();
